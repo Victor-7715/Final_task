472d39f030cefb2cc32f3262cdc5fd7dc5045764
Kozhevnikov Viktor <victor_kozhev@mail.ru>
2022-12-14 01:14:55 +0300

    add task

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Дан массив строк ");
+string[] array = {"1234", ":-)", "col", "-5", "Moscow", "==", "O(n^2)"};
+for (int i = 0; i < array.Length; i++)
+Console.Write(array[i] + " ");
+Console.WriteLine();
+Console.WriteLine("Конечный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
